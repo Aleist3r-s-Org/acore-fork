0e1a5ba48e7ef66055fbca395d7ff06d1f7aae1b
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2023-12-29 17:18:49 +0100

    Monk & Dh spell script files

--- src/server/scripts/Spells/spells_script_loader.cpp
+++ src/server/scripts/Spells/spells_script_loader.cpp
@@ -17,10 +17,12 @@
 
 // This is where scripts' loading functions should be declared:
 void AddSC_deathknight_spell_scripts();
+void AddSC_demonhunter_spell_scripts();
 void AddSC_druid_spell_scripts();
 void AddSC_generic_spell_scripts();
 void AddSC_hunter_spell_scripts();
 void AddSC_mage_spell_scripts();
+void AddSC_monk_spell_scripts();
 void AddSC_paladin_spell_scripts();
 void AddSC_priest_spell_scripts();
 void AddSC_rogue_spell_scripts();
@@ -35,10 +37,12 @@ void AddSC_item_spell_scripts();
 void AddSpellsScripts()
 {
     AddSC_deathknight_spell_scripts();
+    AddSC_demonhunter_spell_scripts();
     AddSC_druid_spell_scripts();
     AddSC_generic_spell_scripts();
     AddSC_hunter_spell_scripts();
     AddSC_mage_spell_scripts();
+    AddSC_monk_spell_scripts();
     AddSC_paladin_spell_scripts();
     AddSC_priest_spell_scripts();
     AddSC_rogue_spell_scripts();
