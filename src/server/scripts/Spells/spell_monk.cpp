4d23991ee7d82cea1c80012c0f3c89b7a458759d
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-01-03 16:12:12 +0100

    base monk spell scripting

--- src/server/scripts/Spells/spell_monk.cpp
+++ src/server/scripts/Spells/spell_monk.cpp
@@ -24,7 +24,170 @@
 
 enum MonkSpells
 {
+    SPELL_MONK_OX_STANCE                    = 101064,
+    SPELL_MONK_OX_STANCE_PASSIVE1           = 101065,
+    SPELL_MONK_OX_STANCE_PASSIVE2           = 101066,
+    SPELL_MONK_SERPENT_STANCE               = 101060,
+    SPELL_MONK_SERPENT_STANCE_PASSIVE       = 101061,
+    SPELL_MONK_TIGER_STANCE                 = 101062,
+    SPELL_MONK_TIGER_STANCE_PASSIVE         = 101063
+};
 
+enum MiscSpells
+{
+    SPELL_BLOOD_ELF_ARCANE_TORRENT_MANA     = 101010,
+    SPELL_BLOOD_ELF_ARCANE_TORRENT_MONK     = 101009,
+    SPELL_BLOOD_ELF_ARCANE_TORRENT_ENERGY   = 101011
+};
+
+// 101009 - Arcane Torrent
+class spell_monk_arcane_torrent : public SpellScript
+{
+    PrepareSpellScript(spell_monk_arcane_torrent);
+
+    bool Validate(SpellInfo const* /*spellEntry*/) override
+    {
+        return ValidateSpellInfo(
+            {
+                SPELL_BLOOD_ELF_ARCANE_TORRENT_MONK,
+                SPELL_BLOOD_ELF_ARCANE_TORRENT_ENERGY,
+                SPELL_MONK_SERPENT_STANCE
+            });
+    }
+
+    void HandleOnHit(SpellEffIndex effIndex)
+    {
+        Unit* caster = GetCaster();
+
+        if (!caster->HasAura(SPELL_MONK_SERPENT_STANCE))
+        {
+            PreventHitDefaultEffect(effIndex);
+            caster->CastSpell(caster, SPELL_BLOOD_ELF_ARCANE_TORRENT_ENERGY, true);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_monk_arcane_torrent::HandleOnHit, EFFECT_1, SPELL_EFFECT_TRIGGER_SPELL);
+    }
+};
+
+// 101062 - Stance of the Sturdy Ox
+class spell_monk_ox_stance_aura : public AuraScript
+{
+    PrepareAuraScript(spell_monk_ox_stance_aura);
+
+    bool Validate(SpellInfo const* /*spellEntry*/) override
+    {
+        return ValidateSpellInfo(
+            {
+                SPELL_MONK_OX_STANCE,
+                SPELL_MONK_OX_STANCE_PASSIVE1,
+                SPELL_MONK_OX_STANCE_PASSIVE2
+            });
+    }
+
+    void HandleOnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        Unit* caster = GetCaster();
+
+        if (!caster->HasAura(SPELL_MONK_OX_STANCE_PASSIVE1))
+            caster->AddAura(SPELL_MONK_OX_STANCE_PASSIVE1, caster);
+
+        if (!caster->HasAura(SPELL_MONK_OX_STANCE_PASSIVE2))
+            caster->AddAura(SPELL_MONK_OX_STANCE_PASSIVE2, caster);
+    }
+
+    void HandleOnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        Unit* caster = GetCaster();
+
+        if (caster->HasAura(SPELL_MONK_OX_STANCE_PASSIVE1))
+            caster->RemoveAura(SPELL_MONK_OX_STANCE_PASSIVE1);
+
+        if (caster->HasAura(SPELL_MONK_OX_STANCE_PASSIVE2))
+            caster->RemoveAura(SPELL_MONK_OX_STANCE_PASSIVE2);
+    }
+
+    void Register() override
+    {
+        OnEffectApply += AuraEffectApplyFn(spell_monk_ox_stance_aura::HandleOnApply, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+        OnEffectRemove += AuraEffectRemoveFn(spell_monk_ox_stance_aura::HandleOnRemove, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+    }
+};
+
+// 101060 - Stance of the Wise Serpent
+class spell_monk_serpent_stance_aura : public AuraScript
+{
+    PrepareAuraScript(spell_monk_serpent_stance_aura);
+
+    bool Validate(SpellInfo const* /*spellEntry*/) override
+    {
+        return ValidateSpellInfo(
+            {
+                SPELL_MONK_SERPENT_STANCE,
+                SPELL_MONK_SERPENT_STANCE_PASSIVE
+            });
+    }
+
+    void HandleOnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        Unit* caster = GetCaster();
+
+        if (!caster->HasAura(SPELL_MONK_SERPENT_STANCE_PASSIVE))
+            caster->AddAura(SPELL_MONK_SERPENT_STANCE_PASSIVE, caster);
+    }
+
+    void HandleOnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        Unit* caster = GetCaster();
+
+        if (caster->HasAura(SPELL_MONK_SERPENT_STANCE_PASSIVE))
+            caster->RemoveAura(SPELL_MONK_SERPENT_STANCE_PASSIVE);
+    }
+
+    void Register() override
+    {
+        OnEffectApply += AuraEffectApplyFn(spell_monk_serpent_stance_aura::HandleOnApply, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+        OnEffectRemove += AuraEffectRemoveFn(spell_monk_serpent_stance_aura::HandleOnRemove, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+    }
+};
+
+// 101062 - Stance of the Fierce Tiger
+class spell_monk_tiger_stance_aura : public AuraScript
+{
+    PrepareAuraScript(spell_monk_tiger_stance_aura);
+
+    bool Validate(SpellInfo const* /*spellEntry*/) override
+    {
+        return ValidateSpellInfo(
+            {
+                SPELL_MONK_TIGER_STANCE,
+                SPELL_MONK_TIGER_STANCE_PASSIVE
+            });
+    }
+
+    void HandleOnApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        Unit* caster = GetCaster();
+
+        if (!caster->HasAura(SPELL_MONK_TIGER_STANCE_PASSIVE))
+            caster->AddAura(SPELL_MONK_TIGER_STANCE_PASSIVE, caster);
+    }
+
+    void HandleOnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+    {
+        Unit* caster = GetCaster();
+
+        if (caster->HasAura(SPELL_MONK_TIGER_STANCE_PASSIVE))
+            caster->RemoveAura(SPELL_MONK_TIGER_STANCE_PASSIVE);
+    }
+
+    void Register() override
+    {
+        OnEffectApply += AuraEffectApplyFn(spell_monk_tiger_stance_aura::HandleOnApply, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+        OnEffectRemove += AuraEffectRemoveFn(spell_monk_tiger_stance_aura::HandleOnRemove, EFFECT_0, SPELL_AURA_MOD_SHAPESHIFT, AURA_EFFECT_HANDLE_REAL);
+    }
 };
 
 void AddSC_monk_spell_scripts()
