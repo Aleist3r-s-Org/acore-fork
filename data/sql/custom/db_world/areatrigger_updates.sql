5978c7df48ecfb2eaba9b6efc947caaca487c9a1
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-01-14 19:58:27 +0100

    Update areatrigger_updates.sql

--- data/sql/custom/db_world/areatrigger_updates.sql
+++ data/sql/custom/db_world/areatrigger_updates.sql
@@ -1,5 +1,5 @@
 DELETE FROM `areatrigger` WHERE `entry` IN (6877, 6878);
 
 INSERT INTO `areatrigger` (`entry`, `map`, `x`, `y`, `z`, `radius`, `length`, `width`, `height`, `orientation`) VALUES
-	(6877, 0, -7567.27, -1329.54, 245.515, 0, 6.56, 1.06, 15, 0.122173),
+	(6877, 0, -7567.27, -1329.54, 245.515, 0, 13.12, 1.06, 15, 0.122173),
 	(6878, 725, -49.097, 0, -2.197, 0, 39.407, 1.06, 19.59, 1.5708);
