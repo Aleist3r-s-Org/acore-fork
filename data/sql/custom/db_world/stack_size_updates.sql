9868f6a619bb2f62ee02c6dc45da090f0f167933
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-02-12 14:43:47 +0100

    Some sql updates (WiP)
    * Moved `stack_size_updates.sql` to `item_template_updates.sql`
    * Some new items and old item changes added

--- data/sql/custom/db_world/stack_size_updates.sql
+++
@@ -1,9 +0,0 @@
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 0) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 3) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 5) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200);
-UPDATE `item_template` SET `stackable` = `stackable` * (1000 / `stackable`) WHERE (`class` = 6) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` < 1000);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 7) AND (`maxcount` < 1) AND (`subclass` >= 1) AND (`stackable` > 1) AND (`stackable` <= 200);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 8) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 13) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 15) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200) AND (`entry` != 22726) AND (`entry` != 50274);
-UPDATE `item_template` SET `stackable` = `stackable` * (200 / `stackable`) WHERE (`class` = 16) AND (`maxcount` < 1) AND (`stackable` > 1) AND (`stackable` <= 200);
